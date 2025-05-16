9d09564b8bacb40594e35241c4e2ef107da23393
Andrei <andrei@literally.dev>
2025-05-16 13:14:08 +0200

    feat: Use zod schema for validating google auth data

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -1,7 +1,18 @@
 import { defineUserSignupFields } from 'wasp/server/auth'
+import { z } from 'zod'
+
+const googleDataSchema = z.object({
+  profile: z.object({
+    email: z.string(),
+    email_verified: z.boolean().optional(),
+  }),
+})
 
 export const userSignupFields = defineUserSignupFields({
-  email: (data: any) => data.profile.email,
+  email: (data) => {
+    const googleData = googleDataSchema.parse(data)
+    return googleData.profile.email
+  },
 })
 
 export function getConfig() {
